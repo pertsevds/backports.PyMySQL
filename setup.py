41c027e0a503c0fa6dc295ab29e0688d2e1d46b2
Dmitriy Pertsev <davaeron@gmail.com>
2022-10-06 23:49:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("./README.rst", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name="backports.PyMySQL",
+    name="backports-PyMySQL",
     version=version,
-    url="https://github.com/pertsevds/backports.PyMySQL",
+    url="https://github.com/pertsevds/backports-PyMySQL",
     project_urls={
         "Documentation": "https://pymysql.readthedocs.io/",
     },
